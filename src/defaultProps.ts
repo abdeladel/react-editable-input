d1a8e54638c9f9c984240cda59fc9519399101d4
Abdeladel <majdeb.abdeladel@gmail.com>
2021-02-15 18:49:05 +0100

    added props types and default props

---
+++ src/defaultProps.ts
@@ -0,0 +1,20 @@
+export const defaultProps = {
+  type: 'string',
+  editable: true,
+  editIn: 'inline',
+  style: {
+    color: 'inherit',
+    background: 'none',
+    border: 'none',
+    textShadow: 'none',
+    display: 'inline-block',
+    cursor: 'default',
+    margin: 0,
+    padding: 0,
+    outline: 0,
+  },
+};
+
+export const defaultPropsEditIcon = {
+  isVisible: false,
+};
---
+++ src/types.ts
@@ -0,0 +1,67 @@
+export type EditableInputProps = {
+  /**
+   * If true it will be editable
+   *
+   * @type {boolean}
+   */
+  editable: boolean;
+
+  /**
+   * Text to be displayed
+   *
+   * @type {string}
+   */
+  text: string;
+
+  /**
+   * Value of the input
+   *
+   * @type {(string | number)}
+   */
+  value?: string | number;
+
+  /**
+   * Type of the input to be edited in
+   *
+   * @type {("string" | "number" | "textarea" | "select" | "multiSelect" | "date" | "color")}
+   */
+  type?:
+    | 'text'
+    | 'number'
+    | 'textarea'
+    | 'select'
+    | 'multiSelect'
+    | 'date'
+    | 'color';
+
+  /**
+   * Function run when the input changes
+   *
+   */
+  onChange?: () => {};
+
+  /**
+   * Indicates how to open the edit component
+   *
+   * @type {("dbclick" | "click" | "hover")}
+   */
+  editType?: 'dbclick' | 'click' | 'hover';
+
+  /**
+   * Indicates how to edit the label
+   *
+   * @type {("inline" | "modal" | "drawer")}
+   */
+  editIn?: 'inline' | 'modal' | 'drawer';
+
+  /**
+   * Custom css attributes styling for the button
+   *
+   * @type {React.CSSProperties}
+   */
+  style?: React.CSSProperties;
+};
+
+export type EditIconProps = {
+  isVisible: boolean;
+};

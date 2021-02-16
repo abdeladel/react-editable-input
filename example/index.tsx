e8f2d1b850e4e974435101fed5b4049a9ff1e47d
Abdeladel <majdeb.abdeladel@gmail.com>
2021-02-16 21:41:03 +0100

    updated example

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,28 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import EditableInput from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <EditableInput
+        style={{ color: 'red' }}
+        editType="hover"
+        text="hover me"
+      />
+      <br />
+      <EditableInput
+        style={{ color: 'red' }}
+        editType="click"
+        text="click me"
+      />
+      <br />
+      <EditableInput
+        style={{ color: 'red' }}
+        editType="dbclick"
+        text="double click me"
+      />
     </div>
   );
 };

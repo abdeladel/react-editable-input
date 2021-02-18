89aa6021188353d506f634fd21e35b7e076c54bf
Abdeladel <majdeb.abdeladel@gmail.com>
2021-02-18 09:08:28 +0100

    updated example

--- example/index.tsx
+++ example/index.tsx
@@ -10,18 +10,21 @@ const App = () => {
         style={{ color: 'red' }}
         editType="hover"
         text="hover me"
+        type="text"
       />
       <br />
       <EditableInput
         style={{ color: 'red' }}
         editType="click"
         text="click me"
+        type="number"
       />
       <br />
       <EditableInput
         style={{ color: 'red' }}
         editType="dbclick"
         text="double click me"
+        type="textarea"
       />
     </div>
   );
